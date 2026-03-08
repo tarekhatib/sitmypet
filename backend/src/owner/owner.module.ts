56bd3a7f02a5f70d7e7094f8493656de17bf86e8
Tarek Khatib <tarekalkhatibb@gmail.com>
2026-03-08 23:40:51 +0200

    bug fix

--- backend/src/owner/owner.module.ts
+++ backend/src/owner/owner.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { StorageModule } from '../storage/storage.module';
+import { ApplicationsModule } from '../applications/applications.module';
 import { OwnerController } from './owner.controller';
 import { OwnerService } from './owner.service';
 
 @Module({
-  imports: [PrismaModule, StorageModule],
+  imports: [PrismaModule, StorageModule, ApplicationsModule],
   controllers: [OwnerController],
   providers: [OwnerService],
 })
